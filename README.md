0810cbcdcbaffd1010609757c76f57ac67aae30c
Nick Ramirez <nickram44@gmail.com>
2018-05-27 14:35:01 -0400

    Fixed missing dns_prefix in aks.tf

--- README.md
+++ README.md
@@ -12,10 +12,20 @@ Usage:
 ~$ terraform apply
 ```
 
-2. Connect to the Kubernetes cluster.
-3. Use `kubectl` to create the Kubernetes resources.
+2. Install the [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
+
+3. Connect to the [Kubernetes cluster](https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough#connect-to-the-cluster)
+
+```
+~$ az login
+~$ az aks get-credentials --resource-group "aks_resourcegroup" --name "akscluster1"
+```
+
+4. Use `kubectl` to create the Kubernetes resources.
 
 ```
 ~$ cd kubernetes
 ~$ kubectl apply -f .
 ```
+
+5. In the Azure Portal, use the link to *View Kubernetes Dashboard* to see the load balanced public IP.
--- terraform/aks.tf
+++ terraform/aks.tf
@@ -19,6 +19,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = "akscluster1"
   location            = "${azurerm_resource_group.aks.location}"
   resource_group_name = "${azurerm_resource_group.aks.name}"
+  dns_prefix          = "dev"
 
   linux_profile {
     admin_username = "aksuser"
@@ -44,28 +45,4 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags {
     Environment = "Dev"
   }
-}
-
-output "id" {
-  value = "${azurerm_kubernetes_cluster.aks.id}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config_raw}"
-}
-
-output "client_key" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_key}"
-}
-
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate}"
-}
-
-output "host" {
-  value = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
 }
